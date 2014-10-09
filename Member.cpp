9535615839e17b3fcfc9318228ed647a135e413d
paul yapobi <yappopaul@hotmail.com>
2014-10-09 03:06:52 -0500

    Update Member.cpp

--- Member.cpp
+++ Member.cpp
@@ -6,11 +6,11 @@
 
 using namespace std; 
 //Constructor
-Member::Member(): md_MemberID(), md_fN (),md_lN(), md_wakeTime(), md_sleepTime(){}
+Member::Member(): md_MemberID(), md_fN (),md_lN(), md_wakeTime(), md_sleepTime(), md_napTime(), md_peakenTime(){}
 
 //Initialiazing data members
-Member::Member  (string fname, string lname, int MemberID, int wakeTime, int sleepTime ) 
-: md_fN(fname), md_lN(lname), md_MemberID(MemberID), md_wakeTime(wakeTime), md_sleepTime(sleepTime) 
+Member::Member  (string fname, string lname, int MemberID, int wakeTime, int sleepTime, int napTime, int peakenTime) 
+: md_fN(fname), md_lN(lname), md_MemberID(MemberID), md_wakeTime(wakeTime), md_sleepTime(sleepTime), md_napTime(napTime), md_peakenTime(peakenTime)
 {
                 removed = false;
 }// Constructor END
@@ -39,6 +39,13 @@ int Member::retwakeTime() {
 int Member::retsleepTime(){
        return md_sleepTime;//return the sleepTime 
        }
+int Member::retnapTime(){
+       return md_napTime;//return napTime
+       }
+int Member::retpeakenTime(){
+       return md_peakenTime;//return peak energy time
+       }
 int Member::retMemberID() {
        return md_MemberID;//return the id
        }
+
