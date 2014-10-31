5b76fc7373cd7cd99ff3b38433e410991cd116ad
mdmmoreno <mariamorenoolivares@my.unt.edu>
2014-10-31 13:07:52 -0500

    Update Member.h

--- Member.h
+++ Member.h
@@ -8,48 +8,33 @@ using namespace std;
 
 
 class Member {
-//the array needs to access this info
-friend class MemberArray;
-
-
-	private:
-
-string md_fN;
-string md_lN;
-int	md_MemberID;
-//circadian settings
-int md_wakeTime;
-int md_sleepTime;
-int md_napTime;
-int md_peakEnergyTime;
-//color settings
-int md_wakeLight;
-int md_sleepLight;
-int md_napLight;
-int md_peakEnergyLight;
-
-
-
 	public:
-           
-Member();
-Member( string fname, string lname, int MemberID, int wakeTime, int sleepTime, int napTime, int peakEnergyTime, int wakeLight, int sleepLight, int napLight, int peakEnergyLight); // Constructor
 
+		string md_fN;
+		string md_lN;
+		int md_MemberID;
 
-bool isId(int MemberID);
-string retfname();
-string retlname();
-int retwakeTime();
-int retsleepTime();
-int retnapTime();
-int retpeakEnergyTime();
-int retMemberID();
-int retwakeLight();
-int retsleepLight();
-int retnapLight();
-int retpeakEnergyLight();
-bool removed; 
+		//circadian settings
+		int md_wakeTime;
+		int md_sleepTime;
+		int md_napTime;
+		int md_peakEnergyTime;
 
+		//color settings
+		int md_wakeLightR;
+		int md_wakeLightG;
+		int md_wakeLightB;
+		int md_sleepLightR;
+		int md_sleepLightG;
+		int md_sleepLightB;
+		int md_napLightR;
+		int md_napLightG;
+		int md_napLightB;
+		int md_peakEnergyLightR;
+		int md_peakEnergyLightG;
+		int md_peakEnergyLightB;
+
+		Member();
 };
 
 #endif
